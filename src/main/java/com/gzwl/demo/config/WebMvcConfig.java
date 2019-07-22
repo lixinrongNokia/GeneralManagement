d4e372ad8d97ecddc89ea5117fd628ba2952d1de
lixinrongNokia <lixinrongnokia@outlook.com>
2019-07-22 21:08:29 +0800

    更换mapper文件位置

--- src/main/java/com/gzwl/demo/config/WebMvcConfig.java
+++ src/main/java/com/gzwl/demo/config/WebMvcConfig.java
@@ -11,15 +11,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @ComponentScan
 public class WebMvcConfig implements WebMvcConfigurer {
-
+	/** 添加静态资源 */
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/image/**").addResourceLocations("/image/");
 		registry.addResourceHandler("/css/**").addResourceLocations("/css/");
 		registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+		registry.addResourceHandler("/console/**").addResourceLocations("/console/");
 		registry.addResourceHandler("/layuiadmin/**").addResourceLocations("/layuiadmin/");
 	}
 
+	/** 跨域名访问 */
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedOrigins("*").allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
